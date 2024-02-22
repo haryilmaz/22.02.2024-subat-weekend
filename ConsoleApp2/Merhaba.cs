8d857963d2635aaac73063dfb6b933ef5bffe355
harun <harun@hotmail.com>
2024-02-22 08:28:14 +0300

    Add project files.

---
+++ ConsoleApp2/Merhaba.cs
@@ -0,0 +1,47 @@
+﻿
+public class Merhaba
+{
+
+    public Task<int> myTask;
+    public Merhaba(Task<string> task)
+    {
+        myTask = GetNumber(task);
+    }
+
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Bir selam ver: ");
+        string Selam = await Selamla();
+        Console.WriteLine($"Al sana selam: {Selam}");
+        Console.WriteLine($"Sen merhaba diyorsan bende senin doğduğun şehrin plakasını veririm:");
+        Task<int> Plaka = GetNumber(Selamla());
+        Console.WriteLine(Plaka);
+    }
+
+    static async Task<string> Selamla()
+    {
+
+        Console.WriteLine("biraz bekle be!");
+        await Task.Delay(2000);
+
+        return "merhaba";
+
+
+    }
+
+    static async Task<int> GetNumber(Task<string> selamTask)
+    {
+        if (Selamla()== selamTask)
+        {
+            
+            return 01;
+        }
+        Console.WriteLine("Selamı doğru dürüst vermezsen sana cevabım İstanbul Plakası olur:");
+        await Task.Delay(1000);
+        
+        return 34;
+    }
+
+
+
+}
---
+++ LearningTask/Merhaba.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningTask
+{
+    public class Merhaba
+    {
+        static async Task Main(string[] args)  
+        {
+            Console.WriteLine("Bir selam ver: ");
+            string Selam = await Selamla();
+            Console.WriteLine($"Al sana selam: {Selam}");
+        }
+
+        static async Task<string> Selamla()
+        {
+
+            Console.WriteLine("biraz bekle be!");
+            await Task.Delay(2000);
+
+            return "merhaba";
+
+
+        }
+             
+
+    }
+}
